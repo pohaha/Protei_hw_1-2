1eda6583663688f645ee4201fc03e1d42c7ae731
Pavel <mouseoskarx7@gmail.com>
2021-08-20 19:02:26 +0300

    code output cleanup

--- main.cpp
+++ main.cpp
@@ -10,8 +10,12 @@ int main()
 {
     Certificate test;
     Fingerprint certificate_fingerprint = test.get_fingerprint();
+    std::cout<<"\ninternal fingerprint output:"<<std::endl;
     certificate_fingerprint.show();  
-    
+    std::cout<<std::endl;
+
     Fingerprint test_fingerprint("../../test_digest.txt");
+    std::cout<<"\nterminal fingerprint output:"<<std::endl;
     test_fingerprint.show();
+    std::cout<<std::endl;
 }
